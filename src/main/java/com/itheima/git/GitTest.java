6eb45b911da7812205e10ffecb844db90533173b
mmzzyy666 <77195152+mmzzyy666@users.noreply.github.com>
2022-06-17 09:36:10 +0800

    commit
    commit

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,git5");
         System.out.println("hello,git7");
         System.out.println("hello,git8");
-        System.out.println("pull,test  }
+        System.out.println("pull,test");  
+    }
 }
